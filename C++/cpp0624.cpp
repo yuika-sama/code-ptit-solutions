f9078e20398ff4f82e3d836472d0ef15e756e6df
Yuika <141085903+yuika-sama@users.noreply.github.com>
2023-12-01 23:14:27 +0700

    Update accepted code

--- C++/cpp0624.cpp
+++ C++/cpp0624.cpp
@@ -8,11 +8,9 @@
 #define keocon {cout << "I used to be your Tinkle Bell, but we're too old for fairytales, my love."}
 using namespace std;
 class sv{
-//	private:
 	public:
-		string name, id, cls, mail;
+		string id, name, cls, mail;
 		friend istream &operator >> (istream &is, sv &a){
-			scanf("\n");
 			getline(cin, a.id);
 			getline(cin, a.name);
 			getline(cin, a.cls);
@@ -20,45 +18,38 @@ class sv{
 			return is;
 		}
 		friend ostream &operator << (ostream &os, sv a){
-			cout << a.id << ' ' << a.name << ' ' << a.cls << ' ' << a.mail << endl;
+			os << a.id << ' ' << a.name << ' ' << a.cls << ' ' << a.mail << endl;
 			return os;
 		}
-		
 };
-
 main(){
-	faster()
-	sv a[1001];
-	int n; 
+	int n, q; 
 	cin >> n;
-	for (int i=0; i<n; i++){
-		cin >> a[i];
-	}
-	int t;
-	cin >> t;
-	while (t--){
-		scanf("\n");
-		string s;
+	sv a[n];
+	cin.ignore();
+	for (auto& x:a) cin >> x;
+	cin >> q;
+	cin.ignore();
+	string s;
+	string major;
+	while (q--){
 		getline(cin, s);
-		
-		string r="";
-		if (s == "Ke toan") r =  "KT";
-	    else if (s == "Cong nghe thong tin") r = "CN";
-	    else if (s == "An toan thong tin") r = "AT";
-	    else if (s == "Vien thong") r = "VT";
-	    else if (s == "Dien tu") r = "DT";
-		for (int i=0; i<s.size(); i++){
-			s[i] = toupper(s[i]);
+		if (s == "Ke toan") major =  "KT";
+	    else if (s == "Cong nghe thong tin") major = "CN";
+	    else if (s == "An toan thong tin") major = "AT";
+	    else if (s == "Vien thong") major = "VT";
+	    else if (s == "Dien tu") major = "DT";
+		for (auto& x:s){
+			x = toupper(x);
 		}
-		
-		cout << "DANH SACH SINH VIEN NGANH " << s << ":\n";
+		cout << "DANH SACH SINH VIEN NGANH " << s << ":" << endl;
 		for (int i=0; i<n; i++){
-			if (r=="CN" || r=="AT"){
-				if (a[i].id[5] == r[0] && a[i].id[6]==r[1] && a[i].cls[0]!='E'){
+			if (major == "CN" or major == "AT"){
+				if (a[i].id[5] == major[0] and a[i].id[6] == major[1] and a[i].cls[0] != 'E'){
 					cout << a[i];
 				}
 			} else {
-				if (a[i].id[5] == r[0] && a[i].id[6]==r[1]){
+				if (a[i].id[5] == major[0] and a[i].id[6] == major[1]){
 					cout << a[i];
 				}
 			}
